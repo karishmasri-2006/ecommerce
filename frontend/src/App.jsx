1b594519de5cc550f79b9806581739c51c6c6631
karishmasri-2006 <kumarshai1973@gmail.com>
2026-05-22 08:13:59 +0530

    fix: add .jsx extensions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
